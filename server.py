99ddf6316ae683238e86f88697e38454bb30e280
Burak Bozdag <bozdagb17@itu.edu.tr>
2021-01-06 17:17:45 +0300

    Perl modules

--- server.py
+++ server.py
@@ -1,8 +1,44 @@
-from flask import Flask,flash, request, jsonify, render_template,Response,redirect,send_from_directory
-
-
+from flask import Flask, flash, request, jsonify, render_template, Response, redirect, send_from_directory
+from perlfunc import perl5lib, perlfunc, perlreq
 
 app = Flask(__name__)
+
+
+@perlfunc
+@perlreq('modules/annotate_variation.pl')
+def annotate_variation():
+    pass
+
+
+@perlfunc
+@perlreq('modules/coding_change.pl')
+def coding_change():
+    pass
+
+
+@perlfunc
+@perlreq('modules/convert2annovar.pl')
+def convert2annovar():
+    pass
+
+
+@perlfunc
+@perlreq('modules/retrieve_seq_from_fasta.pl')
+def retrieve_seq_from_fasta():
+    pass
+
+
+@perlfunc
+@perlreq('modules/table_annovar.pl')
+def table_annovar():
+    pass
+
+
+@perlfunc
+@perlreq('modules/variants_reduction.pl')
+def variants_reduction():
+    pass
+
 
 @app.route("/")
 def index():
@@ -11,6 +47,3 @@ def index():
 
 if __name__ == "__main__":
     app.run()
-
-
-
